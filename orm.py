86deb8b6ec148497b8832c38f2ff46040bace2fe
AndyJCheng <159991981521@163.com>
2018-04-10 23:32:42 +0800

    orm

--- orm.py
+++ orm.py
@@ -16,15 +16,34 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object(config)
 db = SQLAlchemy(app)
+
+
+class Articals(db.Model):
+    __tablename__ = 'Articals'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.TEXT, nullable=False)
+
+
 db.create_all()
 
 
 @app.route('/')
 def index():
-    return 'ok'
+    # artical1 = Articals(title='aa', content='fafdsfasf')
+    # db.session.add(artical1)
+    # db.session.commit()
+
+    # artical1 = Articals.query.filter(Articals.title == 1).all()[0]
+    # artical1 = Articals.query.filter(Articals.title == 'aa').first()
+    # artical1.title = 'new title'
+    # db.session.commit()
+
+    artical1 = Articals.query.filter(Articals.title == 'new title').first()
+    db.session.delete(artical1)
+    db.session.commit()
+    return 'hello'
 
 
 if __name__ == '__main__':
-    app.run()
-
-
+    app.run(port=555)
