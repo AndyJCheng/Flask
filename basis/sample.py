ebc5da2a81c213e6db22f15e111354d3b27115e9
andy cheng <you@example.com>
2018-12-18 00:28:39 +0800

    test credentials

--- basis/sample.py
+++ basis/sample.py
@@ -22,4 +22,4 @@ def greet(name):
 # if __name__ == '__main__':
 #     app.run()
 # app.run()---> flask run
-# how to startup app: 1 set FLASK_APP=sample.py, 2 flask run
+# how to startup app: 1 set FLASK_APP=sample.py, 2 flask run(python -m flask run)
